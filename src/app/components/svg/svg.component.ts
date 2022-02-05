b43752c67ce6677d0b276792ddb9b314516d3cd4
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-02-05 11:15:09 -0300

    first commit

---
+++ src/app/components/svg/svg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg',
+  template: `<h1>Hello svg component</h1>`,
+  styleUrls: ['./svg.component.scss']
+})
+export class SvgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
